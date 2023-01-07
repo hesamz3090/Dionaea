159a76275a2967c8bc194ec6af0a83310fc5b39b
Hesam Aghajani <hesamz3090@gmail.com>
2023-01-07 06:16:34 +0330

    Debug settings.py

--- Dionaea/settings.py
+++ Dionaea/settings.py
@@ -16,7 +16,6 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -27,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = bool(config("DEBUG"))
 
 ALLOWED_HOSTS = list(config("ALLOWED_HOSTS"))
-
+CSRF_TRUSTED_ORIGINS = ['https://www.thedionaea.com', 'https://thedionaea.com']
 
 # Application definition
 JAZZMIN_SETTINGS = {
@@ -139,7 +138,6 @@ INSTALLED_APPS = [
     'website.apps.WebsiteConfig',
 ]
 
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -171,7 +169,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Dionaea.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -181,7 +178,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -201,7 +197,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -212,7 +207,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
