d59a7049cee7ac68539d0672ca1580b6112a5678
hesamz3090 <hesamz3090@gmail.com>
2023-01-12 02:32:06 +0330

    Debug cron.py

--- cron.py
+++ cron.py
@@ -8,43 +8,40 @@ from apps.scan.models import *
 
 start_time = time.time()
 
-tasks = Task.objects.filter(complete=False).order_by('id')[:50]
+tasks = Task.objects.filter(complete=False, scan__status='STARTED').order_by('id')[:50]
 
 for task in tasks:
-    if task.scan.status == 'STOPPED':
-        task.delete()
-    elif task.scan.status == 'STARTED':
-        process = subprocess.Popen(
-            [task.text],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            shell=True
-        )
+    process = subprocess.Popen(
+        [task.text],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True
+    )
 
-        output, err = process.communicate()
-        result = (output + err).decode("utf-8")
-        task_end_time = (time.time() - start_time) / 60
-        if task.command.word in result:
-            task.found = True
+    output, err = process.communicate()
+    result = (output + err).decode("utf-8")
 
-        task.result = result
-        task.complete = True
-        task.time_spend = abs(round((task_end_time - start_time) / 60, 2))
+    if task.command.word in result:
+        task.found = True
 
-        left_task = Task.objects.filter(scan=task.scan, complete=False).count()
-        total_task = Task.objects.filter(scan=task.scan).count()
+    task.result = result
+    task.complete = True
+    task.time_spend = abs(round((time.time() - start_time) / 60, 2))
 
-        if left_task == 0:
-            task.scan.percent = 100
-            task.scan.status = 'COMPLETED'
-            scan_end_time = abs(round((time.time() - start_time) / 60, 2))
+    left_task = Task.objects.filter(scan=task.scan, complete=False).count()
+    total_task = Task.objects.filter(scan=task.scan).count()
 
-        else:
-            task.scan.percent = 100 - ((100 * left_task) / total_task)
+    if left_task == 0:
+        task.scan.percent = 100
+        task.scan.status = 'COMPLETED'
+        scan_end_time = abs(round((time.time() - start_time) / 60, 2))
 
-        task.scan.save()
-        task.save()
+    else:
+        task.scan.percent = 100 - ((100 * left_task) / total_task)
+
+    task.scan.save()
+    task.save()
 
 if tasks.count() == 0:
     websites = Website.objects.filter(status='STARTED')
@@ -52,7 +49,6 @@ if tasks.count() == 0:
         website.status = 'COMPLETED'
         website.percent = 100
         website.save()
-
 
 cron_end_time = abs(round((time.time() - start_time) / 60, 2))
 print(cron_end_time)
